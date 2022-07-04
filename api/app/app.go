94304963cc99af1256e86b0b552a01fb05822316
saihanlinaung <hanlinaung.work@gmail.com>
2022-07-05 01:02:08 +0630

    staging prep

--- api/app/app.go
+++ api/app/app.go
@@ -35,7 +35,8 @@ func (a *App) Initialize(config *configs.Config) {
 
 	//db, err := gorm.Open(config.DB.Dialect, dbURI)
 	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN: dbURI,//configs.DBURL 
+		DSN: configs.DBURL ,
+		//DSN: dbURI,
 		PreferSimpleProtocol: true, // disables implicit prepared statement usage. By default pgx automatically uses the extended protocol
 	  }), &gorm.Config{})
 
--- api/app/models/transaction.go
+++ api/app/models/transaction.go
@@ -10,9 +10,6 @@ type Transaction struct {
 	gorm.Model
 	Uuid		string		`sql:"type:uuid;primary_key;default:uuid_generate_v4()" json:"uuid"`
 	Title   	string      `json:"title"`
-	// CreatedAt	time.Time	`json:"created_at"`
-	// UpdatedAt	time.Time	`json:"updated_at"`
-	// DeletedAt	time.Time	`json:"deleted_at"`
 	WalletId   	string      `json:"wallet_id"`
 	Amount    	string     	`json:"amount"`
 	Category 	string   	`json:"category"`
