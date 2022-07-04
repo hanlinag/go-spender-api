7441258758000047ce666913c5aeff8dfe165427
saihanlinaung <hanlinaung.work@gmail.com>
2022-07-04 22:37:29 +0630

    add configfile

---
+++ api/config/projconfig.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"time"
+)
+
+type Config struct {
+	DB *DBConfig
+}
+
+type DBConfig struct {
+	Host     string
+	Username string
+	Password string
+	Name     string
+	Port 	 string
+}
+
+func GetConfig() *Config {
+	return &Config{
+		DB: &DBConfig{
+			Host:     "localhost",
+			Username: "spender_user",
+			Password: "root",
+			Name:     "spenderdb",
+			Port:  	  "5432",
+		},
+	}
+}
+
+var JWT_SECRET 			= "SpEnDerWeBaPiWiThGoLaNg2022"
+var TokenExpiredTime  = time.Now().Add(1 * time.Minute)
