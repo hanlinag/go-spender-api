67865544627815dd18ddf8d8d438fe79f18a9405
saihanlinaung <hanlinaung.work@gmail.com>
2022-07-07 00:16:21 +0630

    change db

--- config/projconfig.go
+++ config/projconfig.go
@@ -13,7 +13,7 @@ type DBConfig struct {
 	Username string
 	Password string
 	Name     string
-	Port 	 string
+	Port     string
 }
 
 func GetConfig() *Config {
@@ -23,12 +23,11 @@ func GetConfig() *Config {
 			Username: "spender_user",
 			Password: "root",
 			Name:     "spenderdb",
-			Port:  	  "5432",
+			Port:     "5432",
 		},
-
 	}
 }
 
-var JWT_SECRET 			= "SpEnDerWeBaPiWiThGoLaNg2022"
-var DBURL = "postgres://yzimggivagyaph:8cffacdcd5d764252d3aefe2290707ec24914af7b4cea98f234af94d04d8514d@ec2-52-208-164-5.eu-west-1.compute.amazonaws.com:5432/d32htuv5q4469t"
-var TokenExpiredTime  = time.Now().Add(60 * time.Minute)
+var JWT_SECRET = "SpEnDerWeBaPiWiThGoLaNg2022"
+var DBURL = "postgres://ibwwxigahwxcgj:61f5148dac4c00f33d833de69771c6580a7d9941cb53781c08ca8eb530248df3@ec2-99-81-137-11.eu-west-1.compute.amazonaws.com:5432/d1llbgccsbfj9h"
+var TokenExpiredTime = time.Now().Add(60 * time.Minute)
