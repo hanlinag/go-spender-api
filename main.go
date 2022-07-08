69cc5d452ba28f2988111ca79a81fd499a1b5458
saihanlinaung <hanlinaung.work@gmail.com>
2022-07-08 12:17:19 +0630

    change to host port variable

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	app "spender/v1/app"
 	config "spender/v1/config"
 )
@@ -8,7 +10,9 @@ import (
 func main() {
 	config := config.GetConfig()
 
+	port := os.Getenv("PORT")
+
 	app := &app.App{}
 	app.Initialize(config)
-	app.Run(":8080")
+	app.Run(":"+port)
 }
